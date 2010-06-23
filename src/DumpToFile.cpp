f3b729461978efa9d79d4906ba7bb91cebe00c6e
kvahed <kaveh@vahedipour.de>
2010-06-23 09:40:40 +0000

    Checkup and documentation 0.3 Matrix.h

--- src/DumpToFile.cpp
+++ src/DumpToFile.cpp
@@ -3,15 +3,20 @@
 error_code 
 DumpToFile::ProcessData () {
 
-	if (m_have_raw) {
+	if (m_have_raw)
 		m_raw.dump    ((char*) "raw.bin"   );
-		cout << "Dumping raw data" << endl;
-	}
-
 	if (m_have_helper)
 		m_helper.dump ((char*) "helper.bin");
 	if (m_have_pixel)
 		m_pixel.dump  ((char*) "pixel.bin" );
+
+	if (m_labels.length() > 0) {
+	    ofstream of;
+	    of.open ("labels.txt");
+		for (int i = 0; i < m_labels.length(); i++)
+			of << m_labels[i] << "\n";
+	    of.close();
+	}
 
 	return RRSModule::OK;
 
