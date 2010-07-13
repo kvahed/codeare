1afb2e4971475e5205cc44f015899d725fb9cb14
kvahed <kaveh@vahedipour.de>
2010-07-13 16:36:54 +0000

    OMP median dilter

--- src/DumpToFile.cpp
+++ src/DumpToFile.cpp
@@ -3,19 +3,33 @@
 error_code 
 DumpToFile::ProcessData () {
 
-	if (m_have_raw)
+	printf ("Dumping ... ");
+
+	if (m_have_raw) {
+		printf ("raw data.\n");
 		m_raw.dump    ((char*) "raw.bin"   );
-	if (m_have_helper)
+	}
+
+	if (m_have_helper) {
+		printf ("helper data.\n");
 		m_helper.dump ((char*) "helper.bin");
-	if (m_have_pixel)
+	}
+
+	if (m_have_pixel) {
+		printf ("pixel data.\n");
 		m_pixel.dump  ((char*) "pixel.bin" );
+	}
 
 	if (m_labels.length() > 0) {
+		printf ("pixel data.\n");
+
 	    ofstream of;
+
 	    of.open ("labels.txt");
 		for (int i = 0; i < m_labels.length(); i++)
 			of << m_labels[i] << "\n";
 	    of.close();
+
 	}
 
 	return RRSModule::OK;
--- src/MedianFilter.cpp
+++ src/MedianFilter.cpp
@@ -14,8 +14,8 @@ int compare_ints( const void* a, const void* b ) {
 //process the median filter
 RRSModule::error_code MedianFilter::ProcessData () {
 	
-	const int m_ww = 25;
-	const int m_wh = 25;
+	const int m_ww = 16;
+	const int m_wh = 16;
 
 	int ex = (m_ww / 2), ey = (m_wh / 2);
 	int array[m_ww*m_wh];
