29459cbe238fb018e935ed482078dd5f0a8ee6e5
kvahed <kaveh@vahedipour.de>
2010-09-03 19:31:31 +0000

    XGEMM are operational

--- src/Matrix_BLAS.cpp
+++ src/Matrix_BLAS.cpp
@@ -8,42 +8,39 @@ Matrix<T> Matrix<T>::dot (Matrix<T>& M) {
 
 	int i = 0, j = 0, l = 0;
 
-	int  m      = 3;
-	int  n      = 8;
+	int  m      = _dim[LIN];
+	int  n      = M.Dim(COL);
 
-	int  k      = 8;//_dim[1];
+	int  k      = _dim[COL];
 
 	T    alpha  = T(1);
 
 	T*   a      = new T[Size()];
 	i = 0;
 	for (j = 0; j < _dim[COL]; j++)
-		for (l = 0; l < _dim[LIN]; l++, i++) {
+		for (l = 0; l < _dim[LIN]; l++, i++)
 			a[i] = _M[j+l*_dim[COL]];
-		}
 
-	int  lda    = 3;//_dim[0];
+	int  lda    = _dim[LIN];//_dim[0];
 
 	T*   b      = new T[M.Size()];
 	i = 0;
 	for (j = 0; j < M.Dim(COL); j++)
-		for (l = 0; l < M.Dim(LIN); l++, i++) {
+		for (l = 0; l < M.Dim(LIN); l++, i++)
 			b[i] = M[j+l*M.Dim(COL)];
-			std::cout << b[i] << std::endl;
-		}
 
-	int  ldb    = 8;//M.Dim(0);
+	int  ldb    = M.Dim(LIN);//M.Dim(0);
 
 	T    beta   = T(0);
 
 
-	res.Dim(0)  = 3;//_dim[0];
-	res.Dim(1)  = 8;//M.Dim(1);
+	res.Dim(0)  = M.Dim(COL);//_dim[0];
+	res.Dim(1)  = _dim[LIN];//M.Dim(1);
 	res.Reset();
 	
 
 	T*   c      = new T[res.Size()];
-	int  ldc    = res.Dim(1);
+	int  ldc    = _dim[LIN];
 
 	if (typeid(T) == typeid(double))
 		dgemm_ (&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc);
@@ -51,9 +48,10 @@ Matrix<T> Matrix<T>::dot (Matrix<T>& M) {
 		cgemm_ (&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc);
 
 	i = 0;
-	for (i = 0; i < res.Size(); i++)
-		res[i] = c[i];
-		
+	for (j = 0; j < res.Dim(COL); j++)
+		for (l = 0; l < res.Dim(LIN); l++, i++)
+			res[j+l*res.Dim(COL)] = c[i];
+
 	delete [] a;
 	delete [] b;
 	delete [] c;
--- src/modules/LapackTests.cpp
+++ src/modules/LapackTests.cpp
@@ -1,6 +1,6 @@
 /*
  *  jrrs Copyright (C) 2007-2010 Kaveh Vahedipour
- *                               Forschungszentrum Jülich, Germany
+ *                               Forschungszentrum Juelich, Germany
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -63,14 +63,9 @@ LapackTests::Process     () {
 	test.Reset();
 	test.Random();
 
-	std::cout << test;
-
+	std::cout << "Testing mm multiplication (dgemm) for helper:     ";
 	Matrix<double> product = test.dot(m_helper);
 	
-	printf("-----------------------\n");
-
-	std::cout << product;
-
 	return RRSModule::OK;
 
 }
