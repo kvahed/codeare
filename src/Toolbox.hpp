39085a477c54333003b2ca12b92fa91c4ac79b0f
kvahed <kaveh@vahedipour.de>
2011-04-15 14:33:37 +0000

    CGSENSE

--- src/Toolbox.hpp
+++ src/Toolbox.hpp
@@ -4,22 +4,15 @@
 #include <vector>
 #include <string>
 
-void split (std::vector<std::string>& sv, const std::string& str, const std::string& dlm) {
+class Toolbox {
 
-	assert (dlm.size() > 0);
+public:
 
-	size_t  start = 0, end = 0;
+	Toolbox() {};
+	~Toolbox(){};
 
-	while (end != std::string::npos) {
+	void split (std::vector<std::string>& sv, const std::string& str, const std::string& dlm);
 
-		end = str.find (dlm, start);
-
-		// If at end, use length=maxLength.  Else use length=end-start.
-		sv.push_back(str.substr(start, (end == std::string::npos) ? std::string::npos : end - start));
-
-		// If at end, use start=maxSize.  Else use start=end+delimiter.
-		start = ((end > (std::string::npos - dlm.size())) ? std::string::npos : end + dlm.size());
-	}
 };
 
 #endif
