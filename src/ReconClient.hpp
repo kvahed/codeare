b3e5f9d46975c270f0f3d54e27b5547ac1edcc4d
kvahed <kaveh@vahedipour.de>
2011-07-20 10:06:41 +0000

    Reducing memory foot print and in between storing on client and server side

--- src/ReconClient.hpp
+++ src/ReconClient.hpp
@@ -99,26 +99,7 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void 
-		SetRaw              (Matrix< std::complex<float> >& M) {
-			
-			raw_data r; 
-
-			r.dims.length(INVALID_DIM);
-			
-			for (int j = 0; j < INVALID_DIM; j++)
-				r.dims[j] = M.Dim(j);
-			
-			r.dreal.length(M.Size()); 
-			r.dimag.length(M.Size());
-			
-			for (int i = 0; i < M.Size(); i++) {
-				r.dreal[i] = M[i].real();
-				r.dimag[i] = M[i].imag(); 
-			}
-			
-			m_rrsi->raw(r);
-	
-		};
+		SetRaw              (Matrix< std::complex<float> >& M);
 		
 		
 		/**
@@ -127,19 +108,7 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void 
-		GetRaw              (Matrix< std::complex<float> >& M) {
-			
-			raw_data* rp = m_rrsi->raw();
-
-			for (int j = 0; j < INVALID_DIM; j++)
-				M.Dim(j) = rp->dims[j];
-			
-			M.Reset();
-			
-			for (int i = 0; i < GetSize(rp->dims); i++)
-				M[i] = std::complex<float>(rp->dreal[i],rp->dimag[i]);
-			
-		};
+		GetRaw              (Matrix< std::complex<float> >& M);
 		
 		
 		/**
@@ -148,26 +117,7 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void 
-		SetRHelper              (Matrix< std::complex<float> >& M) {
-			
-			raw_data r;
-
-			r.dims.length(INVALID_DIM);
-			
-			for (int j = 0; j < INVALID_DIM; j++)
-				r.dims[j] = M.Dim(j);
-			
-			r.dreal.length(M.Size()); 
-			r.dimag.length(M.Size());
-			
-			for (int i = 0; i < M.Size(); i++) {
-				r.dreal[i] = M[i].real();
-				r.dimag[i] = M[i].imag(); 
-			}
-			
-			m_rrsi->rhelper(r);
-			
-		};
+		SetRHelper          (Matrix< std::complex<float> >& M);
 		
 		
 		/**
@@ -176,19 +126,7 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void 
-		GetRHelper              (Matrix< std::complex<float> >& M) {
-			
-			raw_data* rp = m_rrsi->rhelper();
-
-			for (int j = 0; j < INVALID_DIM; j++)
-				M.Dim(j) = rp->dims[j];
-			
-			M.Reset();
-			
-			for (int i = 0; i < GetSize(rp->dims); i++)
-				M[i] = std::complex<float>(rp->dreal[i],rp->dimag[i]);
-			
-		};
+		GetRHelper          (Matrix< std::complex<float> >& M);
 		
 		
 		/**
@@ -197,23 +135,7 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void
-		SetHelper              (Matrix< double >& M) {
-			
-			helper_data h;
-
-			h.dims.length(INVALID_DIM);
-			
-			for (int j = 0; j < INVALID_DIM; j++)
-				h.dims[j] = M.Dim(j);
-			
-			h.vals.length(M.Size());
-			
-			for (int i = 0; i < M.Size(); i++)
-				h.vals[i] = M[i];
-			
-			m_rrsi->helper(h);
-			
-		};
+		SetHelper           (Matrix< double >& M);
 		
 		
 		/**
@@ -222,19 +144,7 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void
-		GetHelper              (Matrix< double >& M) {
-			
-			helper_data* hp = m_rrsi->helper();
-
-			for (int j = 0; j < INVALID_DIM; j++)
-				M.Dim(j) = hp->dims[j];
-			
-			M.Reset();
-			
-			for (int i = 0; i < GetSize(hp->dims); i++)
-				M[i] = hp->vals[i];
-			
-		};
+		GetHelper           (Matrix< double >& M);
 		
 		
 		/**
@@ -243,23 +153,7 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void
-		SetKSpace              (Matrix< double >& M) {
-			
-			helper_data h;
-
-			h.dims.length(INVALID_DIM);
-			
-			for (int j = 0; j < INVALID_DIM; j++)
-				h.dims[j] = M.Dim(j);
-			
-			h.vals.length(M.Size());
-			
-			for (int i = 0; i < M.Size(); i++)
-				h.vals[i] = M[i];
-			
-			m_rrsi->kspace(h);
-			
-		};
+		SetKSpace           (Matrix< double >& M);
 		
 		
 		/**
@@ -268,19 +162,7 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void
-		GetKSpace              (Matrix< double >& M) {
-			
-			helper_data* hp = m_rrsi->kspace();
-
-			for (int j = 0; j < INVALID_DIM; j++)
-				M.Dim(j) = hp->dims[j];
-			
-			M.Reset();
-			
-			for (int i = 0; i < GetSize(hp->dims); i++)
-				M[i] = hp->vals[i];
-			
-		};
+		GetKSpace           (Matrix< double >& M);
 		
 		
 		/**
@@ -289,23 +171,7 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void 
-		SetPixel            (Matrix<short>& M) {
-			
-			pixel_data p;
-
-			p.dims.length(INVALID_DIM);
-			
-			for (int j = 0; j < INVALID_DIM; j++)
-				p.dims[j] = M.Dim(j);
-			
-			p.vals.length(M.Size()); 
-			
-			for (int i = 0; i < M.Size(); i++)
-				p.vals[i] = M[i];
-			
-			m_rrsi->pixel(p);
-			
-		};
+		SetPixel            (Matrix<short>& M);
 		
 		
 		/**
@@ -314,23 +180,11 @@ namespace RRClient {
 		 * @param  M        Given matrix
 		 */
 		void 
-		GetPixel            (Matrix<short>& M) {
-			
-			pixel_data* pp = m_rrsi->pixel();
-
-			for (int j = 0; j < INVALID_DIM; j++)
-				M.Dim(j) = pp->dims[j];
-			
-			M.Reset();
-			
-			for (int i = 0; i < GetSize(pp->dims); i++)
-				M[i] = pp->vals[i];
-			
-		};
+		GetPixel            (Matrix<short>& M);
 		
 		
 		/**
-		 * @brief           Raw repository size
+		 * @brief           Get size from dimensions
 		 *
 		 * @return          Size
 		 */
@@ -348,61 +202,5 @@ namespace RRClient {
 	
 }
 
-
-/**
- * @brief Corba communication exception
- */
-class DS_ServerConnectionException {
-public:
-	/**
-	 * @brief Handle communication exceptions
-	 */
-	DS_ServerConnectionException () { 
-		std::cerr << "CORBA COMM_FAILURE" << std::endl; 
-	};
-};
-
-
-/**
- * @brief Corba system exception
- */
-
-class DS_SystemException           {
-public:
-	/**
-	 * @brief Handle system exceptions
-	 */
-	DS_SystemException           () { 
-		std::cerr << "CORBA Exception" << std::endl; 
-	};
-};
-
-
-/**
- * @brief Corba fatal exception
- */
-class DS_FatalException            {
-public:
-	/**
-	 * @brief Handle fatal exception
-	 */
-	DS_FatalException            () { 
-		std::cerr << "CORBA Fatal Exception" << std::endl; 
-	};
-};
-
-
-/**
- * @brief Corba unspecified exception
- */
-class DS_Exception                 {
-public:
-	/**
-	 * @brief Handle unspecified exception
-	 */
-	DS_Exception                 () { 
-		std::cerr << "Exception" << std::endl; 
-	};
-};
 
 #endif // __RECON_CLIENT_H__
