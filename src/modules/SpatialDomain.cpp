7176c768807d0f83db250e0af79816ce06a180c6
kvahed <kaveh@vahedipour.de>
2011-07-28 10:27:57 +0000

    Spatial domain first checkin

--- src/modules/SpatialDomain.cpp
+++ src/modules/SpatialDomain.cpp
@@ -75,7 +75,7 @@ RFLimits                      (const Matrix<raw>* solution, const int* pd, const
  */
 Matrix<raw>
 Magnetisation           (const Matrix<double>* ks, const Matrix<double>* r, const Matrix<raw>* b1, const Matrix<short>* b0, 
-						 const int             nc, const int            nk, const int          ns, const int            gd);
+                         const int             nc, const int            nk, const int          ns, const int            gd);
 
 
 
@@ -84,32 +84,32 @@ SpatialDomain::SpatialDomain  () {}
 
 
 SpatialDomain::~SpatialDomain () {
-	
+    
 }
 
 
 RRSModule::error_code
 SpatialDomain::Init           ()  {
 
-	RRSModule::error_code e = OK;
+    RRSModule::error_code e = OK;
 
-	Attribute ("Nk",      &m_nk); // # kt points
+    Attribute ("Nk",      &m_nk); // # kt points
 
-	m_pd = (int*) malloc (m_nk * sizeof(int));
-	
-	Attribute ("pd",      &m_pd[0]);   // Pulse durations    [10us]
+    m_pd = (int*) malloc (m_nk * sizeof(int));
+    
+    Attribute ("pd",      &m_pd[0]);   // Pulse durations    [10us]
 
-	for (int i = 1; i < m_nk; i++)
-		m_pd[i] = m_pd[0];
+    for (int i = 1; i < m_nk; i++)
+        m_pd[i] = m_pd[0];
 
-	Attribute ("gd",      &m_gd);      // Gradient durations [10us]
-	Attribute ("Ns",      &m_ns);      // # of spatial sites
-	Attribute ("Nc",      &m_nc);      // # of transmit channels
-	Attribute ("maxiter", &m_maxiter); // Max # of iterations
-	Attribute ("lambda",  &m_lambda);  // # Tikhonov parameter
-	Attribute ("rflim",   &m_rflim);   // # RF limit (usualy in [0 1])
+    Attribute ("gd",      &m_gd);      // Gradient durations [10us]
+    Attribute ("Ns",      &m_ns);      // # of spatial sites
+    Attribute ("Nc",      &m_nc);      // # of transmit channels
+    Attribute ("maxiter", &m_maxiter); // Max # of iterations
+    Attribute ("lambda",  &m_lambda);  // # Tikhonov parameter
+    Attribute ("rflim",   &m_rflim);   // # RF limit (usualy in [0 1])
    
-	return e;
+    return e;
 
 }
 
@@ -117,9 +117,9 @@ SpatialDomain::Init           ()  {
 RRSModule::error_code
 SpatialDomain::Finalise       ()  {
 
-	free (m_pd);
+    free (m_pd);
 
-	return OK;
+    return OK;
 
 }
 
@@ -127,93 +127,93 @@ SpatialDomain::Finalise       ()  {
 RRSModule::error_code
 SpatialDomain::Process        () { 
 
-	// On entry -------------------
-	//
-	// m_raw:     target pattern
-	// m_kspace:  kt points
-	// m_pixel:   b0 (Hz)
-	// m_rhelper: b1+ maps
-	// m_helper:  spatial positions
-	// ----------------------------
+    // On entry -------------------
+    //
+    // m_raw:     target pattern
+    // m_kspace:  kt points
+    // m_pixel:   b0 (Hz)
+    // m_rhelper: b1+ maps
+    // m_helper:  spatial positions
+    // ----------------------------
 
-	// On exit --------------------
-	// 
-	// m_raw:    Pulses
-	// m_helper: Pulse durations
-	// ----------------------------
-	
-	Matrix<raw> treg         = Matrix<raw>::id(m_nc * m_nk) * raw (m_lambda, 0);
+    // On exit --------------------
+    // 
+    // m_raw:    Pulses
+    // m_helper: Pulse durations
+    // ----------------------------
+    
+    Matrix<raw> treg         = Matrix<raw>::id(m_nc * m_nk) * raw (m_lambda, 0);
 
-	bool        pulse_amp_ok = false;
-	float*      max_rf       = (float*) malloc (m_nk * sizeof(float));
+    bool        pulse_amp_ok = false;
+    float*      max_rf       = (float*) malloc (m_nk * sizeof(float));
 
-	Matrix<raw> solution;
-	Matrix<raw> tmp;
-	Matrix<raw> final;	
-	
-	// Start clock ------------------------
-	ticks cgstart = getticks();
+    Matrix<raw> solution;
+    Matrix<raw> tmp;
+    Matrix<raw> final;    
+    
+    // Start clock ------------------------
+    ticks cgstart = getticks();
 
-	while (!pulse_amp_ok) {
+    while (!pulse_amp_ok) {
 
-		Matrix<raw> m    = Magnetisation (&m_kspace, &m_helper, &m_rhelper, &m_pixel, m_nc, m_nk, m_ns, m_gd);
-		Matrix<raw> minv = m.tr();
+        Matrix<raw> m    = Magnetisation (&m_kspace, &m_helper, &m_rhelper, &m_pixel, m_nc, m_nk, m_ns, m_gd);
+        Matrix<raw> minv = m.tr();
 
-		minv *= m;
-		minv += treg;
-		minv  = minv.Inv();	
-		minv.dotc(m);
-		
-		// Valriable exchange method --------------
+        minv *= m;
+        minv += treg;
+        minv  = minv.Inv();    
+        minv.dotc(m);
+        
+        // Valriable exchange method --------------
 
-		float err = 1e6;
-		std::vector<double> res;
-		
-		printf ("Variable exchange method ...\n");
+        float err = 1e6;
+        std::vector<double> res;
+        
+        printf ("Variable exchange method ...\n");
 
-		for (int j = 0; j < m_maxiter; j++) {
-			
-			solution = minv * m_raw;
-			tmp      = m    * solution;
+        for (int j = 0; j < m_maxiter; j++) {
+            
+            solution = minv * m_raw;
+            tmp      = m    * solution;
 
-			res.push_back (NRMSE (&m_raw, &tmp));
-			
-			if (res.at(j) > err) break;
-			if (res.at(j) < 2.5) break;
-			
-			err	     = res.at(j) - 1.0e-3;
-			
-			final    = solution;
-			m_raw    = PhaseCorrection (&m_raw, &tmp);
-			
-		}	
-		
-		printf ("... done. Checking pulse amplitudes ... \n");
-		
-		// Check max pulse amplitude -----------------
-		RFLimits (&final, m_pd, m_nk, m_nc, max_rf);
-		pulse_amp_ok = true;
+            res.push_back (NRMSE (&m_raw, &tmp));
+            
+            if (res.at(j) > err) break;
+            if (res.at(j) < 2.5) break;
+            
+            err         = res.at(j) - 1.0e-3;
+            
+            final    = solution;
+            m_raw    = PhaseCorrection (&m_raw, &tmp);
+            
+        }    
+        
+        printf ("... done. Checking pulse amplitudes ... \n");
+        
+        // Check max pulse amplitude -----------------
+        RFLimits (&final, m_pd, m_nk, m_nc, max_rf);
+        pulse_amp_ok = true;
 
-		for (int i = 0; i < m_nk; i++) 
-			if (max_rf[i] > m_rflim)
-				pulse_amp_ok = false;
-		
-		// Update Pulse durations if necessary -------
-		if (!pulse_amp_ok) {
-			
-			printf ("... done. Pulse amplitudes to high! Updating pulse durations ...\n");
-			
-			for(int i=0; i < m_nk;i++)
-				m_pd[i] = 1 + (int) (max_rf[i] * m_pd[i] / m_rflim);
+        for (int i = 0; i < m_nk; i++) 
+            if (max_rf[i] > m_rflim)
+                pulse_amp_ok = false;
+        
+        // Update Pulse durations if necessary -------
+        if (!pulse_amp_ok) {
+            
+            printf ("... done. Pulse amplitudes to high! Updating pulse durations ...\n");
+            
+            for(int i=0; i < m_nk;i++)
+                m_pd[i] = 1 + (int) (max_rf[i] * m_pd[i] / m_rflim);
 
-		}
-		
-		printf ("... done\n.");
+        }
+        
+        printf ("... done\n.");
 
-	}
+    }
 
-	free (max_rf);
-	return RRSModule::OK;
+    free (max_rf);
+    return RRSModule::OK;
 
 }
 
@@ -222,17 +222,17 @@ SpatialDomain::Process        () {
 float      
 NRMSE                         (const Matrix<raw>* target, const Matrix<raw>* result) {
 
-	float q = 0;
-	float n = 0;
+    float q = 0;
+    float n = 0;
 
-	for (int i = 0; i < target->Size(); i++)
-		q += pow (abs(target->at(i)) - abs(result->at(i)), 2.0);
+    for (int i = 0; i < target->Size(); i++)
+        q += pow (abs(target->at(i)) - abs(result->at(i)), 2.0);
 
-	q = sqrt(q)/target->norm().real();
+    q = sqrt(q)/target->norm().real();
 
-	printf ("MRMSE: %.3f\n", q);
+    printf ("MRMSE: %.3f\n", q);
 
-	return q * 100.0;
+    return q * 100.0;
 
 }
 
@@ -241,67 +241,66 @@ NRMSE                         (const Matrix<raw>* target, const Matrix<raw>* res
 Matrix<raw> 
 PhaseCorrection               (const Matrix<raw>* target, const Matrix<raw>* result) {
 
-	Matrix<raw> tmp = *target;
-	
+    Matrix<raw> tmp = *target;
+    
 #pragma omp parallel default (shared) 
-	{
-		
-		int tid      = omp_get_thread_num();
-		int chunk    = tmp.Size() / omp_get_num_threads();
-		
+    {
+        
+        int tid      = omp_get_thread_num();
+        int chunk    = tmp.Size() / omp_get_num_threads();
+        
 #pragma omp for schedule (dynamic, chunk)
-		
-		for (int i = 0; i < tmp.Size(); i++) 
-			if (abs(target->at(i)) > 0)	
-				tmp[i] = abs (target->at(i)) * result->at(i) / abs(result->at(i));
-			else				
-				tmp[i] = raw (0.0,0.0);	
+        
+        for (int i = 0; i < tmp.Size(); i++) 
+            if (abs(target->at(i)) > 0)    
+                tmp[i] = abs (target->at(i)) * result->at(i) / abs(result->at(i));
+            else                
+                tmp[i] = raw (0.0,0.0);    
 
-	}
-	
-	return tmp;
-	
+    }
+    
+    return tmp;
+    
 }
 
 
 void 
 RFLimits            (Matrix<raw> solution, float* pd, int nk, int nc, float* limits) {
-	
-	for (int i = 0; i < nk; i++) {
-		
-		limits[i] = 0.0;
-		
-		for (int j = 0; j < nc; j++)
-			if (limits[i] < abs (solution [i+nk*j]) / pd[i]) 
-				limits[i] = abs (solution [i+nk*j]) / pd[i];
+    
+    for (int i = 0; i < nk; i++) {
+        
+        limits[i] = 0.0;
+        
+        for (int j = 0; j < nc; j++)
+            if (limits[i] < abs (solution [i+nk*j]) / pd[i]) 
+                limits[i] = abs (solution [i+nk*j]) / pd[i];
 
-	}
-		
+    }
+        
 }
 
 
 Matrix< std::complex<float> >
 Magnetisation (const Matrix<double>* ks, const Matrix<double>* r, const Matrix< std::complex<float> >* b1, const Matrix<short>* b0, int nc, int nk, int ns, int gd) {
-	
-	Matrix< std::complex<float> > mxy;
-	mxy.Dim(COL) = ns;
-	mxy.Dim(LIN) = nc;
-	mxy.Reset();
-	
-	std::complex<float> pgd = std::complex<float> (0, 2.0 * PI * GAMMA * GRAD_RASTER);            // 2* i * \pi * \gamma * \delta t
-	
-	for (int c = 0; c < nc; c++) 
-		for (int k = 0; k < nk; k++) 
-			for (int s = 0; s < ns; s++) {
-				mxy[c*nk + k*ns + s]  = 
-					pgd * b1->at(c*s + s) *                   // b1 (s,c) 
-					exp (raw(0, 2.0 * PI * gd * b0->at(s))) *  // delta b0:        exp (ib0t) 
-					exp (raw(0,(ks->at(0,k) * r->at(0,s) + 
-								ks->at(1,k) * r->at(1,s) + 
-								ks->at(2,k) * r->at(2,s))));   // kspace encoding: exp (ik(t)r)
-			}
-	
-	return mxy;
+    
+    Matrix< std::complex<float> > mxy;
+    mxy.Dim(COL) = ns;
+    mxy.Dim(LIN) = nc;
+    mxy.Reset();
+    
+    std::complex<float> pgd = std::complex<float> (0, 2.0 * PI * GAMMA * GRAD_RASTER);            // 2* i * \pi * \gamma * \delta t
+    
+    for (int c = 0; c < nc; c++) 
+        for (int k = 0; k < nk; k++) 
+            for (int s = 0; s < ns; s++)
+                mxy[c*nk + k*ns + s]  = 
+                    pgd * b1->at(c*s + s) *                   // b1 (s,c) 
+                    exp (raw(0, 2.0 * PI * gd * b0->at(s))) *  // delta b0:        exp (ib0t) 
+                    exp (raw(0,(ks->at(0,k) * r->at(0,s) + 
+                                ks->at(1,k) * r->at(1,s) + 
+                                ks->at(2,k) * r->at(2,s))));   // kspace encoding: exp (ik(t)r)
+    
+    return mxy;
 
 }
 
@@ -317,6 +316,6 @@ extern "C" DLLEXPORT ReconStrategy* create  ()                 {
 
 extern "C" DLLEXPORT void           destroy (ReconStrategy* p) {
 
-	delete p;
+    delete p;
 
 }
