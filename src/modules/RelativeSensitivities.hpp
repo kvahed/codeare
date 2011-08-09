dd73f170203e7a43dddbfeb8abe80934530ad69f
kvahed <kaveh@vahedipour.de>
2011-08-09 15:56:26 +0000

    reading raw

---
+++ src/modules/RelativeSensitivities.hpp
@@ -0,0 +1,84 @@
+/*
+ *  jrrs Copyright (C) 2007-2010 Kaveh Vahedipour
+ *                               Forschungszentrum Jülich, Germany
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+ *  02110-1301  USA
+ */
+
+#ifndef __RELATIVE_SENSITIVITIES_HPP__
+#define __RELATIVE_SENSITIVITIES_HPP__
+
+#include "ReconStrategy.hpp"
+
+using namespace RRServer;
+
+
+/**
+ * @brief Reconstruction startegies
+ */
+namespace RRStrategy {
+
+	/**
+	 * @brief Empty recon for test purposes
+	 */
+	class RelativeSensitivities : public ReconStrategy {
+		
+		
+	public:
+		
+		/**
+		 * @brief Default constructor
+		 */
+		RelativeSensitivities  () {}
+		
+		/**
+		 * @brief Default destructor
+		 */
+		virtual 
+		~RelativeSensitivities () {}
+		
+		
+		/**
+		 * @brief Do nothing 
+		 */
+		virtual RRSModule::error_code
+		Process ();
+		
+		/**
+		 * @brief Do nothing 
+		 */
+		virtual RRSModule::error_code
+		Init ();
+		
+		/**
+		 * @brief Do nothing 
+		 */
+		virtual RRSModule::error_code
+		Finalise ();
+
+
+
+	private:
+
+		
+		double m_echo_shift;
+		
+		
+	};
+
+}
+#endif /* __RELATIVE_SENSITIVITIES_H__ */
+
