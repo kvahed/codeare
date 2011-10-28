177ff758da5fd1651e57d9b6709d8db53011023a
kvahed <kaveh@vahedipour.de>
2011-10-28 20:24:41 +0000

    OpenCLing

--- src/modules/OpenCLUtil.hpp
+++ src/modules/OpenCLUtil.hpp
@@ -1,13 +1,12 @@
 //from http://www.songho.ca/opengl/gl_vbo.html
 
-#ifndef __OPENCL_UTIL_H__
-#define __OPENCL_UTIL_H__
+#ifndef ADVCL_UTIL_H_INCLUDED
+#define ADVCL_UTIL_H_INCLUDED
 
-char*
-file_contents  (const char *filename, int *length);
+char *file_contents(const char *filename, int *length);
 
-const char* 
-oclErrorString (cl_int error);
+//const char* oclErrorString(cl_int error);
 
-#endif // __OPENCL_UTIL_H__
+
+#endif
 
