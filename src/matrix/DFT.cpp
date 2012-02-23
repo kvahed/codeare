f18bec0e9076c0373cfc349a08be85514b09705a
Kaveh Vahedipour <k.vahedipour@fz-juelich.de>
2012-02-23 12:01:14 +0100

    New DFT class for more elaborate FFT

---
+++ src/matrix/DFT.cpp
@@ -0,0 +1,171 @@
+/*
+ *  jrrs Copyright (C) 2007-2010 Kaveh Vahedipour
+ *                               Forschungszentrum Juelich, Germany
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301  USA
+ */
+
+#include "DFT.hpp"
+#include "FFT.hpp"
+#include "Algos.hpp"
+
+#include <fftw3.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+DFT::DFT (const Matrix<int> size, const Matrix<double> mask, const Matrix<double> pc) : m_N(1),
+																						m_have_mask (false),
+																						m_have_pc (false) {
+	
+	int rank = size.Size();
+	int n[rank];
+
+	
+	if (mask.Size() > 1) {
+		m_have_mask = true;
+		m_mask = mask;
+	}
+	
+	if (pc.Size() > 1) {
+		m_have_pc = true;
+		m_pc = pc;
+	}
+
+	for (size_t i = 0; i < rank; i++) {
+		n[i]  = size[rank-1-i];
+		m_N  *= n[i];
+	}
+
+	m_in  = (fftwf_complex*) fftwf_malloc (sizeof(fftwf_complex) * m_N);
+	m_out = (fftwf_complex*) fftwf_malloc (sizeof(fftwf_complex) * m_N);
+
+	m_fwdplanf = fftwf_plan_dft (rank, n, m_in, m_out, FFTW_FORWARD,  FFTW_MEASURE);
+	m_bwdplanf = fftwf_plan_dft (rank, n, m_in, m_out, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+	m_initialised = true;
+
+}
+
+
+DFT::~DFT () {
+
+	fftwf_destroy_plan (m_fwdplanf);
+	fftwf_destroy_plan (m_bwdplanf);
+
+	fftwf_cleanup ();
+
+	fftwf_free(m_in); 
+	fftwf_free(m_out);
+
+}
+
+
+template<> Matrix<cxfl> 
+DFT::Trafo (Matrix<cxfl>& m) const {
+	
+    Matrix<cxfl> res = FFT::Shift(m);
+	
+	memcpy (m_in, &res[0], sizeof(fftwf_complex) * m_N);
+
+	fftwf_execute(m_fwdplanf);
+
+	memcpy (&res[0], m_out, sizeof(fftwf_complex) * m_N);
+
+	if (m_have_mask)
+		res *= m_mask;
+
+    return FFT::Shift(res/sqrt((float)m.Size()));
+	
+}
+
+
+template<> Matrix<cxfl>
+DFT::Adjoint (Matrix<cxfl>& m) const {
+
+	assert (Algos::Is1D(m) || Algos::Is2D(m) || Algos::Is3D(m));
+	
+    Matrix<cxfl> res = FFT::Shift(m);
+	fftwf_plan   p; 
+	
+	if      (Algos::Is1D(m))
+		p = fftwf_plan_dft_1d (m.Dim(0),                     (fftwf_complex*)&res[0], (fftwf_complex*)&res[0], FFTW_BACKWARD, FFTW_ESTIMATE);
+	else if (Algos::Is2D(m))
+		p = fftwf_plan_dft_2d (m.Dim(1), m.Dim(0),           (fftwf_complex*)&res[0], (fftwf_complex*)&res[0], FFTW_BACKWARD, FFTW_ESTIMATE);
+	else if (Algos::Is3D(m))
+		p = fftwf_plan_dft_3d (m.Dim(2), m.Dim(1), m.Dim(0), (fftwf_complex*)&res[0], (fftwf_complex*)&res[0], FFTW_BACKWARD, FFTW_ESTIMATE);
+	
+	fftwf_execute(p);
+	fftwf_destroy_plan(p);
+
+	return FFT::Shift(res/sqrt((float)m.Size()));
+	
+}
+
+
+template<> Matrix<cxdb> 
+DFT::Trafo (Matrix<cxdb>& m) const {
+	
+	assert (Algos::Is1D(m) || Algos::Is2D(m) || Algos::Is3D(m));
+	
+    Matrix<cxdb> res;
+	fftw_plan    p;
+
+	res = FFT::Shift(m);
+
+	if      (Algos::Is1D(m))
+		p = fftw_plan_dft_1d (m.Dim(0),                     (fftw_complex*)&res[0], (fftw_complex*)&res[0], FFTW_FORWARD, FFTW_ESTIMATE);
+	else if (Algos::Is2D(m))
+		p = fftw_plan_dft_2d (m.Dim(1), m.Dim(0),           (fftw_complex*)&res[0], (fftw_complex*)&res[0], FFTW_FORWARD, FFTW_ESTIMATE);
+	else if (Algos::Is3D(m))
+		p = fftw_plan_dft_3d (m.Dim(2), m.Dim(1), m.Dim(0), (fftw_complex*)&res[0], (fftw_complex*)&res[0], FFTW_FORWARD, FFTW_ESTIMATE);
+	
+	fftw_execute(p);
+	fftw_destroy_plan(p);
+
+    return FFT::Shift(res / (float)res.Size());
+	
+}
+
+
+template<> Matrix<cxdb>
+DFT::Adjoint (Matrix<cxdb>& m) const {
+
+	assert (Algos::Is1D(m) || Algos::Is2D(m) || Algos::Is3D(m));
+	
+    Matrix<cxdb> res;
+	fftw_plan    p; 
+	
+	res = FFT::Shift(m);
+
+	if      (Algos::Is1D(m))
+		p = fftw_plan_dft_1d (m.Dim(0),                     (fftw_complex*)&res[0], (fftw_complex*)&res[0], FFTW_BACKWARD, FFTW_ESTIMATE);
+	else if (Algos::Is2D(m))
+		p = fftw_plan_dft_2d (m.Dim(1), m.Dim(0),           (fftw_complex*)&res[0], (fftw_complex*)&res[0], FFTW_BACKWARD, FFTW_ESTIMATE);
+	else if (Algos::Is3D(m))
+		p = fftw_plan_dft_3d (m.Dim(2), m.Dim(1), m.Dim(0), (fftw_complex*)&res[0], (fftw_complex*)&res[0], FFTW_BACKWARD, FFTW_ESTIMATE);
+	
+	fftw_execute(p);
+	fftw_destroy_plan(p);
+
+	return FFT::Shift(res);
+	
+}
+
+
+
+
