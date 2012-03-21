2e94991ada7008f6e7151ef7007d63e3b107db06
Kaveh Vahedipour <k.vahedipour@fz-juelich.de>
2012-03-21 10:39:09 +0100

    Syngo MR IO test added. Relative sensitivities and bet2 under investigation

---
+++ src/tests/syngo.hpp
@@ -0,0 +1,11 @@
+#include "matrix/IO.hpp"
+
+template <class T> bool
+syngotest (Connector<T>* rc) {
+
+		std::string in = std::string (base + std::string (data));
+		Matrix<cxfl> meas;
+		RAWRead (meas, in, "VB15");
+
+
+}
