15f4a9a909555d668357f133f52ddf602175526c
Kaveh Vahedipour <k.vahedipour@fz-juelich.de>
2012-06-08 22:17:51 +0200

    Adding preliminary SENSE operator

---
+++ src/modules/SENSE.cpp
@@ -0,0 +1,34 @@
+#include "SENSE.hpp"
+
+using namespace RRStrategy;
+
+RRSModule::error_code 
+SENSE::Init () {
+
+	return RRSModule::OK;
+
+}
+
+
+RRSModule::error_code 
+SENSE::Prepare () {
+
+	return RRSModule::OK;
+
+}
+
+
+RRSModule::error_code
+SENSE::Process () { 
+
+	return RRSModule::OK;
+
+}
+
+
+RRSModule::error_code
+SENSE::Finalise () { 
+
+	return RRSModule::OK;
+
+}
