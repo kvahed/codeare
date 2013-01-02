2ce7a5e78092ca1e7633e5e97dbc7f524c0ec7f7
Kaveh Vahedipour <kaveh@vahedipour.de>
2013-01-02 14:28:55 +0100

    Matrix constructor check

--- src/modules/RelativeSensitivities.cpp
+++ src/modules/RelativeSensitivities.cpp
@@ -94,7 +94,7 @@ RelativeSensitivities::Process     () {
 
     // Do we have GRE for segmentation? --------
 	
-    Matrix<short>& bets = AddMatrix ("bets", (Ptr<Matrix<short> >) NEW (Matrix<short> (mask.Dim())));
+    Matrix<short>& bets = AddMatrix ("bets", (Ptr<Matrix<short> >) NEW (Matrix<short> (mask.DimVector())));
 	
     if (m_use_bet == 1) { // Better test? / Replace with SNRO?
 		
@@ -103,7 +103,7 @@ RelativeSensitivities::Process     () {
 
 		SOS (mask, ndims(mask)-1);
         
-        Matrix<double> bet(mask.Dim());
+        Matrix<double> bet(mask.DimVector());
         double         tmp = 0.0;
 		
         for (size_t i = 0; i < numel(mask); i++) {
