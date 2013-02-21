80d8a09da95e5265c39e1c5366b0272d890056cd
kvahed <kaveh@vahedipour.de>
2013-02-21 16:32:30 +0100

    MPI stuff

---
+++ src/matrix/linalg/t_inv.cpp
@@ -0,0 +1,30 @@
+#include "Matrix.hpp"
+#include "Creators.hpp"
+#include "Lapack.hpp"
+
+template<class T> void inv_check () {
+
+    Matrix<T> A = rand<T>(4,4);
+
+#ifdef VERBOSE
+    std::cout << "A=\n" << A;
+#endif
+    
+    A  = inv (A);
+    
+#ifdef VERBOSE
+    std::cout << "inv(A)=\n" << A << std::endl;
+#endif
+
+}
+
+int main (int args, char** argv) {
+
+    inv_check<cxfl>();
+    inv_check<cxdb>();
+    inv_check<float>();
+    inv_check<double>();
+    
+    return 0;
+    
+}
