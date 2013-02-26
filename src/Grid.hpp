60a4885f133f4c599e843602373e3994a2d219d5
kvahed <kaveh@vahedipour.de>
2013-02-26 14:04:27 +0100

    Grid concept into its own class

---
+++ src/Grid.hpp
@@ -0,0 +1,49 @@
+#ifndef __GRID_HPP__
+#define __GRID_HPP__
+
+#include <ostream>
+
+/**
+ * @brief BLACS grid 
+ */
+class Grid {
+    
+public:
+    
+	int  np; /**< @brief # of processes */
+	int  rk; /**< @brief my rank        */
+	int  ct; /**< @brief context        */
+	int  nr; /**< @brief # of proc rows */
+	int  nc; /**< @brief # of proc columns */
+	int  mr; /**< @brief my row # */
+	int  mc; /**< @brief my col # */
+	char order; /**< @brief row/col major */
+    
+    const char* c_str() const;
+    
+    static Grid* Instance();
+
+    virtual ~Grid();
+    
+private:
+    
+    Grid ();
+    static Grid* m_inst;
+    
+};
+
+
+/**
+ * @brief            Dump to ostream
+ *
+ * @param  os        Output stream
+ * @param  w         Workspace
+ * @return           The output stream
+ */
+inline static std::ostream&
+operator<< (std::ostream& os, Grid& g) {
+	os << g.c_str();
+    return os;
+}
+
+#endif //__GRID_HPP__
