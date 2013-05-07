27a47d6e3773908f6f24a85e38e637fdd0d9845b
kvahed <kaveh@vahedipour.de>
2013-05-07 16:38:34 +0200

    ignore file

--- src/matrix/arithmetic/t_emul.cpp
+++ src/matrix/arithmetic/t_emul.cpp
@@ -5,8 +5,8 @@
 template<class T>
 void emul_check () {
 
-    Matrix<T> A = rand<T>(3,4);
-    Matrix<T> B = rand<T>(3,4);
+    Matrix<T> A = rand<T>(4,2);
+    Matrix<T> B = rand<T>(4,2);
     T a = T(2.0);
     Matrix<T> C, D;
 
