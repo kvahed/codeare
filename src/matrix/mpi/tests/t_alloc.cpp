8de8da068a07e9d6ddb7662a8c77c8d95e7c03b9
kvahed <kaveh@vahedipour.de>
2013-06-24 23:00:10 +0200

    More MPI

---
+++ src/matrix/mpi/tests/t_alloc.cpp
@@ -0,0 +1,35 @@
+#include "Grid.hpp"
+#include "PMatrix.hpp"
+#include "Print.hpp"
+#include "PIO.hpp"
+#include "Algos.hpp"
+#include "Creators.hpp"
+
+#include <math.h>
+#include <iostream>
+
+
+
+template<class T> bool
+check_alloc() {
+
+    Grid& gd = *Grid::Instance();
+	Matrix<T,MPI> A (16,16);
+    A = rand<T,uniform>(size(A));
+    std::cout << A << std::endl;
+    return true;
+
+}
+
+
+int main (int args, char** argv) {
+
+    Grid& gd = *Grid::Instance();
+    check_alloc<float>();
+    //check_alloc<double>();
+    //check_alloc<cxfl>();
+    // check_alloc<cxdb>();
+    
+    return 0;
+
+}
