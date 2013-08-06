6be7473d3619f9bdceacf7887dfc611985ff8a89
kvahed <kaveh@vahedipour.de>
2013-08-06 23:11:11 +0200

    Merge step for documentation

--- src/reconserver.cpp
+++ src/reconserver.cpp
@@ -50,57 +50,33 @@ char*  logfile;
 using namespace std;
 using namespace RRServer;
 
+static const std::string head = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s";
+
 bool init (int argc, char** argv);
 
-static int begin_request_handler(struct mg_connection *conn) {
-    const struct mg_request_info *request_info = mg_get_request_info(conn);
-    char content[100];
+inline static int req_handler (struct mg_connection *conn) {
 
+    mg_get_request_info(conn);
     std::stringstream wd;
-    
     wd << wspace;
+    std::string ws = wd.str();
     
-    // Prepare the message we're going to send
-    int content_length = snprintf(content, sizeof(content),
-                                  "codeare service workspace dump:\n %s",
-                                  wd.str().c_str());
+    mg_printf(conn, head.c_str(), ws.length(), ws.c_str());
     
-    // Send HTTP reply to the client
-    mg_printf(conn,
-              "HTTP/1.1 200 OK\r\n"
-              "Content-Type: text/plain\r\n"
-              "Content-Length: %d\r\n"        // Always set Content-Length
-              "\r\n"
-              "%s",
-              content_length, content);
-    
-    // Returning non-zero tells mongoose that our function has replied to
-    // the client, and mongoose should not send client any more data.
     return 1;
     
 }
 
-void httpServer () {
+inline static void mongoose_service () {
     
     struct mg_context *ctx;
     struct mg_callbacks callbacks;
-    
-    // List of options. Last element must be NULL.
     const char *options[] = {"listening_ports", "8080", NULL};
     
-    // Prepare callbacks structure. We have only one callback, the rest are NULL.
     memset(&callbacks, 0, sizeof(callbacks));
-    callbacks.begin_request = begin_request_handler;
-    
-    // Start the web server.
+    callbacks.begin_request = req_handler;
     ctx = mg_start(&callbacks, NULL, options);
-    std::cout <<  "In" << std::endl;
-    // Wait until user hits "enter". Server is running in separate thread.
-    // Navigating to http://localhost:8080 will invoke begin_request_handler().
     getchar();
-    
-    std::cout <<  "Out" << std::endl;
-    // Stop the server.
     mg_stop(ctx);
     
 }
@@ -109,11 +85,11 @@ int main (int argc, char** argv) {
 
     if (!init (argc, argv))
         return 1;
-    
-    boost::thread t(httpServer);
-    t.detach();
 
-    
+    // Webserver for worspace view
+    boost::thread bt (mongoose_service);
+    bt.detach();
+
     // --------------------------------------------------------------------------
     // Start CORBA server:
     // --------------------------------------------------------------------------
