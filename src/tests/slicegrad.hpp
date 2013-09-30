b4b8cf5e5e1cba8bd7fdca1d5e741dca4966be70
kvahed <kaveh@vahedipour.de>
2013-09-30 17:07:58 +0200

    slicegrad.hpp missing

---
+++ src/tests/slicegrad.hpp
@@ -0,0 +1,29 @@
+#include "../modules/GradientTiming.hpp"
+
+template <class T> bool
+slicegrad (Connector<T>* rc) {
+
+	GradientParams gp;
+    
+    IOContext fin (rc->GetElement("/config/data-in"), base, READ);
+    gp.k = fin.Read<double>(rc->GetElement("/config/data-in/kin"));
+    fin.Close();
+
+	rc->Attribute ("maxgrad", &(gp.mgr));
+	rc->Attribute ("maxslew", &(gp.msr));
+	rc->Attribute ("dt",      &(gp.dt));
+	
+	rc->Attribute ("gunits",  &(gp.gunits));
+	rc->Attribute ("lunits",  &(gp.lunits));
+
+	//Matrix<double> x  = linspace<double> (0.0,1.0,size(gp.k,0));
+	//Matrix<double> xi = linspace<double> (0.0,1.0,size(gp.k,0)*hpoints);
+	//gp.k = k;//interp1 (x, gp.k, xi, INTERP::AKIMA);
+
+    Solution s = ComputeGradient (gp);
+
+    IOContext fout (rc->GetElement("/config/data-out"), base, WRITE);
+    s.dump (fout);
+    fout.Close();
+
+}
