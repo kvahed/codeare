b84cdbf211fb6f82a5ca4d3c550826621f65bb7a
kvahed <kaveh@vahedipour.de>
2014-02-11 13:09:22 +0100

    CMaking on

---
+++ src/matrix/AssignmentHandler.hpp
@@ -0,0 +1,28 @@
+/*
+ * AssignmentHandler.hpp
+ *
+ *  Created on: Nov 21, 2013
+ *      Author: kvahed
+ */
+
+#ifndef ASSIGNMENTHANDLER_HPP_
+#define ASSIGNMENTHANDLER_HPP_
+
+template<class T>
+class AssignmentHandler {
+
+public:
+	inline AssignmentHandler (Matrix<T>& M) : _M(M) {
+
+	}
+
+	inline AssignmentHandler& operator= (const Matrix<T>& M) {
+		return *this;
+	}
+
+protected:
+	Matrix<T>& _M;
+};
+
+
+#endif /* ASSIGNMENTHANDLER_HPP_ */
