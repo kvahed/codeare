20f90d69c3387e857e0f433c480bc3b50dedc36b
Kaveh Vahedipour <kaveh@codeare.org>
2014-02-14 10:07:21 +0100

    Dependencies for libtiff and libpng in  cmake

--- src/matrix/linalg/tests/t_dot.cpp
+++ src/matrix/linalg/tests/t_dot.cpp
@@ -2,29 +2,34 @@
 #include "Algos.hpp"
 #include "Creators.hpp"
 #include "Lapack.hpp"
+#include "Print.hpp"
 
-template<class T> void dot_check () {
+#define VERBOSE
 
-    Matrix<T> A = rand<T> (4,1);
-    Matrix<T> B = rand<T> (4,1);
-    
+template<class T> bool dot_check () {
+
+	size_t n = 4;
+    Matrix<T> x = rand<T> (n,1);
+    Matrix<T> y = rand<T> (n,1);
+	T a = dot(x,y), b = dotc(x,y), c = 0., d = 0.; 
+
+	for (size_t i = 0; i < n; ++i) {
+		c += x[i]*y[i];
+		d += TypeTraits<T>::Conj(x[i])*y[i];
+	}
+
 #ifdef VERBOSE
-    std::cout << "A=\n" << A;
-    std::cout << "B=\n" << B;
-    std::cout << "A * B=\n" << dot(A,B) << std::endl;
-    std::cout << "A**H * B=\n" << dotc(A,B) << std::endl;
+    std::cout << "x=\n" << x << "y=\n" << y;
+    std::cout << "y * y=\n" << a << " " << c << std::endl;
+    std::cout << "x**H * y=\n" << b << " " << d << "\n" << std::endl;
 #endif
+
+	return (TypeTraits<T>::Abs((a-c)/a)<1.e-6 && 
+			TypeTraits<T>::Abs((b-d)/b)<1.e-6);
 
 }
 
-
 int main (int args, char** argv) {
-
-    dot_check<cxfl>();
-    dot_check<cxdb>();
-    dot_check<float>();
-    dot_check<double>();
-    
-    return 0;
-    
+	return (dot_check<cxfl>() && dot_check<cxdb>() && 
+			dot_check<float>() && dot_check<double>()) ? 0 : 1;
 }
--- src/matrix/linalg/tests/t_norm.cpp
+++ src/matrix/linalg/tests/t_norm.cpp
@@ -6,29 +6,27 @@
 
 #define VERBOSE
 
-template<class T> void norm_check () {
+template<class T> bool norm_check () {
 
     Matrix<T> A = rand<T> (4,3);
+	double nrm1 = norm(A), nrm2 = 0.;
+
+	for (size_t i = 0; i < A.Size(); ++i)
+		nrm2 += TypeTraits<T>::Real(A[i]*TypeTraits<T>::Conj(A[i]));
+	nrm2 = sqrt(nrm2);
     
 #ifdef VERBOSE
     std::cout << "A=\n" << A;
-#endif
-    
-
-#ifdef VERBOSE
-    std::cout << "norm(A)=" << norm(A) << std::endl;
+    std::cout << "norm(A)=" << nrm1 << " " << nrm2 << std::endl;
 #endif
 
+	return ((nrm1-nrm2)/nrm1 < 1.e-6);
 
 }
 
 int main (int args, char** argv) {
 
-    norm_check<cxfl>();
-    norm_check<cxdb>();
-    norm_check<float>();
-    norm_check<double>();
-    
-    return 0;
-    
+	return  (norm_check<cxfl>()  && norm_check<cxdb>() && 
+			 norm_check<float>() && norm_check<double>()) ? 0 : 1;
+		
 }
