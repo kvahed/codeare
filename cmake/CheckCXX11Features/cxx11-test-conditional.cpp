dadb28c6844c0482e8e9553e3b6b678924f94c85
Kaveh Vahedipour <kaveh@codeare.org>
2015-06-16 11:29:20 -0400

    C++11 std::conditional improper test fixed

--- cmake/CheckCXX11Features/cxx11-test-conditional.cpp
+++ cmake/CheckCXX11Features/cxx11-test-conditional.cpp
@@ -1,8 +1,17 @@
 #include <type_traits>
 #include <string>
 
-int main() {
+template<class T> class A {
+public: 
 	typedef typename std::conditional<false, const std::string, std::string>::type StringType;
+	A() : s(""), t(0) {}
+	virtual ~A () {}
+private: 
     StringType s;
+	T t;
+};
+
+int main() {
+	A<float> a;
 	return 0;
 }
