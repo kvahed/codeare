0930aadebeb6a25f7da331f89513dc3b88b1bdbd
Kaveh Vahedipour <kaveh@codeare.org>
2015-09-04 11:34:47 -0400

    simd on scalar operators

---
+++ src/matrix/tests/t_ediv.cpp
@@ -0,0 +1,27 @@
+#include <Matrix.hpp>
+#include <Creators.hpp>
+#include <Print.hpp>
+
+template<class T> inline static int check () {
+    Matrix<T> A = randn<T>(3,4), B = A, C = A, D = randn<T>(1,1);
+    T a = D[0];
+    B /= a;
+    C /= A;
+    std::cout << "A = [" <<std::endl;
+    std::cout << A << "];" << std::endl;
+    std::cout << a << "/A" <<std::endl;
+    std::cout << a/A << std::endl;
+    std::cout << "A/" << a  <<std::endl;
+    std::cout << A/a << std::endl;
+    std::cout << "A/=" << a <<std::endl;
+    std::cout << B << std::endl;
+    std::cout << "A/A" <<std::endl;
+    std::cout << A/A << std::endl;
+    std::cout << "A/=A" <<std::endl;
+    std::cout << C << std::endl;
+    return 0;
+}
+
+int main (int args, char** argv) {
+    return check<float>() + check<cxfl>() + check<double>() + check<cxdb>();
+}
