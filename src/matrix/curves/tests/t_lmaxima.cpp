2ad128e809d849ab18da62e7fe801b12b595db0b
kvahed <kaveh@vahedipour.de>
2015-10-21 15:34:57 -0400

    lowess smoothing

---
+++ src/matrix/curves/tests/t_lmaxima.cpp
@@ -0,0 +1,14 @@
+#include <LocalMaxima.hpp>
+#include <Trigonometry.hpp>
+
+template<class T> int check () {
+    using namespace codeare::matrix::arithmetic;
+    Matrix<T> t = linspace<T>(0., 2*PI-1e-5, 512);
+    Matrix<T> A = sin(t) + sin(8*t);
+    loc_max_t<T> lm = findLocalMaxima(A);
+    return 0;
+}
+
+int main (int narg, const char** argv) {
+    return check<float>() + check<double>();
+}
