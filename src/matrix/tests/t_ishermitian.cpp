79af6eb2290d99530efa23980eebac796de66236
kvahed <kaveh@vahedipour.de>
2015-10-23 23:27:04 -0400

    eigenvalues for symmetric/hermitian matrices optimised

---
+++ src/matrix/tests/t_ishermitian.cpp
@@ -0,0 +1,23 @@
+#include <Matrix.hpp>
+#include <Creators.hpp>
+#include <Symmetry.hpp>
+#include <Lapack.hpp>
+#include <Print.hpp>
+
+template<class T> inline static int check () {
+    Matrix<T> A = randn<T>(3,3);
+    std::cout << "A = [" << std::endl;
+    std::cout << A << "];" << std::endl;
+    std::cout << "issymmtric(A): " ;
+    std::cout << ishermitian(A) << std::endl << std::endl;
+    A = gemm(A,A,'N','C');
+    std::cout << "A*A' = [" << std::endl;
+    std::cout << A << "];" << std::endl;
+    std::cout << "issymmtric(A*A'): " ;
+    std::cout << ishermitian(A) << std::endl << std::endl;
+    return 0;
+}
+
+int main (int args, char** argv) {
+    return check<cxfl>() + check<cxdb>();
+}
