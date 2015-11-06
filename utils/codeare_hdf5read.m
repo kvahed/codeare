cb97162df4cc75e6c64d69829632cb0d3a526a01
kvahed <kaveh@vahedipour.de>
2015-11-05 19:53:31 -0500

    matlab read/write for hdf5 file

--- utils/codeare_hdf5read.m
+++ utils/codeare_hdf5read.m
@@ -1,32 +1,36 @@
-function [] = codeare_hdf5read (fname)
+function [] = load_ch5 (fname)
+%LOAD_CH5 Reads data from HDF5 files.
 %    
 %   Read codeare hdf5 output.
 %   
-%   [data,names] = codeare_hdf5read (fname);
+%   codeare_hdf5read (fname);
 %
 %   IN: 
 %     fname: File name
-%   OUT: 
-%     data:  Cell array of matrices
-%     names: The named under which the data was saved in HDF5 file
 %   
 %   Kaveh Vahedipour - Research Centre Juelich, 2013
     
-    hinfo = hdf5info(fname);
-    nsets = numel(hinfo.GroupHierarchy.Datasets);
-    dsets = hinfo.GroupHierarchy.Datasets;
-    data  = cell(nsets);
+    hinfo = h5info(fname);
+    nsets = numel(hinfo.Datasets);
+    dsets = hinfo.Datasets;
     names = cell(nsets);
     
     %display (sprintf('   reading %d datasets ...', nsets));
     
     for i = 1:nsets
-        names{i} = dsets(i).Name(2:end);
-        %display (sprintf('     %s', names{i}));
-        tmp = hdf5read (dsets(i));
+        names{i} = dsets(i).Name;
+        cplx = 0;
+        if numel(dsets(i).Attributes)>0
+            for j=1:numel(dsets(i).Attributes);
+                if (strcmp(dsets(i).Attributes(j).Name,'complex'))
+                    cplx = dsets(i).Attributes(j).Value;
+                end
+            end
+        end 
+        tmp = h5read (fname,['/' names{i}]);
         tsz = size(tmp);
         tn  = numel(tmp);
-        if (tsz(1) == 2)
+        if (tsz(1) == 2 && cplx == 1)
             tmp = tmp(:);
             tmp = tmp(1:2:end)+1i*tmp(2:2:end);
             if (numel(tsz)==2)
@@ -38,7 +42,7 @@ function [] = codeare_hdf5read (fname)
     end
 
     clear tmp names tsz tn data dsets nsets hinfo cmd i fname;
-    whos
+    
     %display ('   done.');
     
 end
--- utils/codeare_hdf5write.m
+++ utils/codeare_hdf5write.m
@@ -1,14 +1,18 @@
-function [] = codeare_hdf5write (varargin) 
+function [] = save_ch5 (varargin) 
     fname = varargin{1};
     display(sprintf('filename = %s',fname));
     delf = sprintf('rm %s', fname);
     [~,~] = system(delf);
     for i=2:nargin
         vname = sprintf('/%s',inputname(i));
-        if (iscomplex(varargin{i}))
+        cplx  = iscomplex(varargin{i});
+        if (cplx)
             varargin{i} = reshape([real(varargin{i}(:))';imag(varargin{i}(:))'],[2 size(varargin{i})]);
         end
-        h5create (fname,vname,size(varargin{i}));
+        h5create (fname,vname,size(varargin{i}),'Datatype',class(varargin{i}));
         h5write (fname,vname,varargin{i});
+        if (cplx)
+            h5writeatt(fname,vname,'complex',1);
+        end
     end
 end
